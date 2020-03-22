2dff671d45c0ea48484fefb9ae6f7ff6351512bb
Lyan Martins <lyan.martins3@gmail.com>
2020-03-22 16:25:42 -0300

    added validation class

--- src/lib/data.php
+++ src/lib/data.php
@@ -1,8 +1,8 @@
 <?php
 namespace ModelPhp\Lib;
-use ModelPhp\Lib\Db\Connection;
+use ModelPhp\Lib\Db\Validation;
 
-class Data extends Connection
+class Data extends Validation
 {
     public $table;
 
---
+++ src/lib/db/Validation.php
@@ -0,0 +1,12 @@
+<?php
+namespace ModelPhp\Lib\Db;
+
+
+class Validation extends Connection
+{
+    public function __construct(){
+        var_dump("valida");
+        Parent::__construct();
+        
+    }   
+}
